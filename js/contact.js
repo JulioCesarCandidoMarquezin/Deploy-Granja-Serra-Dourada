a8e7d66597e01ff0719d6e52d00183a66af42c37
Tsutski <melquimendes40@gmail.com>
2024-11-25 14:14:46 -0400

    Verifica se dados válidos foram colocados no formulário

--- js/contact.js
+++ js/contact.js
@@ -6,6 +6,9 @@ const btn_cancel = document.querySelector("#btn-cancel");
 const blocks = document.querySelectorAll(".content");
 const email_box = document.querySelector("#email-box");
 
+var form = document.forms["mail-form"].elements;
+const sub_button = document.querySelector("#sub-button");
+
 btn_loc.addEventListener("click", () => {
     if (mapa.style.display === 'none') {
         mapa.style.display = 'flex';
@@ -39,3 +42,37 @@ btn_cancel.addEventListener("click", () => {
         btn_loc.style.display = 'flex';
     })
 })
+
+function validate() {
+    sub_button.disabled = true;
+
+    var valid = true;
+
+    for (var i = 0; i < form.length; i++){
+        
+        if(form[i].type === "text"){
+
+           if (form[i].value.length == 0){
+                valid = false;
+           } 
+
+        }
+
+        if(form[i].type === "number"){
+
+            if (isNaN(form[i].value) || form[i].value.length == 0){
+                 valid = false;
+            } 
+         }
+
+         if(form[i].type === "checkbox"){
+
+            if (!form[i].checked){
+                 valid = false;
+            } 
+         }        
+        }
+        
+        sub_button.disabled = !valid;
+}
+
