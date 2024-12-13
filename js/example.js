17b7f1e5db8df464767084101a4d391bcc6e360c
JulioCesarCandidoMarquezin <juliocesarmarquezin@gmail.com>
2024-12-13 16:51:05 -0400

    Removido script desnecessário

--- js/example.js
+++
@@ -1,9 +0,0 @@
-const btn_login = document.querySelector("#btn-login");
-const btn_cad = document.querySelector("#btn_cad");
-const login = document.querySelector("#login");
-const cad = document.querySelector("#cadastro");
-
-btn_cad.addEventListener("click", ()  =>{
-    login.style.display = "none"; 
-    cad.style.display = "flex";
-})
