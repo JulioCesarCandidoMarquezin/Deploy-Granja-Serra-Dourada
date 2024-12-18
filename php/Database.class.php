91270eeba0556f2e4bf019c7147dd9a6df6b5782
JulioCesarCandidoMarquezin <juliocesarmarquezin@gmail.com>
2024-12-18 09:21:16 -0400

    Port

--- php/Database.class.php
+++ php/Database.class.php
@@ -5,7 +5,7 @@ final class Database
     private static string $driver = "pgsql"; 
     private static string $host = "dpg-cthckg9opnds73b27ic0-a"; 
     private static string $dbname = "granja_serra_dourada"; 
-    private static int $port = 3306; 
+    private static int $port = 5432; 
     private static string $username = "granja_serra_dourada_user"; 
     private static string $password = "XrAuvQhiE2WJeoXnnrh4F1VAd0KQCgmB"; 
     private static string $charset = "utf8mb4"; 
