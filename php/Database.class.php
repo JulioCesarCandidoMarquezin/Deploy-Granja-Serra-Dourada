a9c5568172d97a3ab6cc4a7c3f2a1c730afeb396
JulioCesarCandidoMarquezin <juliocesarmarquezin@gmail.com>
2024-12-18 09:15:13 -0400

    Docker para deploy

--- php/Database.class.php
+++ php/Database.class.php
@@ -2,12 +2,12 @@
 
 final class Database
 {
-    private static string $driver = "mysql"; 
-    private static string $host = "mysql"; 
-    private static string $dbname = "granja-serra-dourada"; 
+    private static string $driver = "pqsql"; 
+    private static string $host = "dpg-cthckg9opnds73b27ic0-a"; 
+    private static string $dbname = "granja_serra_dourada"; 
     private static int $port = 3306; 
-    private static string $username = "root"; 
-    private static string $password = "root"; 
+    private static string $username = "granja_serra_dourada_user"; 
+    private static string $password = "XrAuvQhiE2WJeoXnnrh4F1VAd0KQCgmB"; 
     private static string $charset = "utf8mb4"; 
     private static ?PDO $pdo = null;
 
